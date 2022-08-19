e69b4fc674b3e9c73cd2d2ae2c2996f94472488d
Elie Kattour <ekattour1@gmail.com>
2022-08-19 19:06:30 +0300

    homepage

--- lib/views/screen/home_screen.dart
+++ lib/views/screen/home_screen.dart
@@ -1,10 +1,82 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:ecommerce_using_getx/utils/theme.dart';
+import 'package:ecommerce_using_getx/views/widget/HomePage/card_item.dart';
+import 'package:ecommerce_using_getx/views/widget/text_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../widget/HomePage/searchformtext.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: context.theme.backgroundColor,
+          body: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 180,
+                decoration: BoxDecoration(
+                  color: Get.isDarkMode ? darkGreyClr : mainColor,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextUtils(
+                          text: "Find Your",
+                          fontSize: 25,
+                          fontweight: FontWeight.bold,
+                          color: Colors.white,
+                          underline: TextDecoration.none),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      TextUtils(
+                          text: "INSPIRATION",
+                          fontSize: 28,
+                          fontweight: FontWeight.bold,
+                          color: Colors.white,
+                          underline: TextDecoration.none),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SearchFormText(),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: TextUtils(
+                      text: "Categories",
+                      fontSize: 20,
+                      fontweight: FontWeight.bold,
+                      color: Get.isDarkMode ? Colors.white : Colors.black,
+                      underline: TextDecoration.none),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              CardItem(),
+            ],
+          )),
+    );
   }
 }
---
+++ lib/views/widget/HomePage/searchformtext.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SearchFormText extends StatelessWidget {
+  const SearchFormText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: TextInputType.text,
+      cursorColor: Colors.black,
+      decoration: InputDecoration(
+        fillColor: Colors.white,
+        focusColor: Colors.red,
+        prefixIcon: Icon(
+          Icons.search,
+          color: Colors.grey,
+        ),
+        hintText: "Search your looking for",
+        hintStyle: TextStyle(
+          color: Colors.black54,
+          fontSize: 15,
+          fontWeight: FontWeight.w500,
+        ),
+        filled: true,
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+            borderRadius: BorderRadius.circular(10)),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+            borderRadius: BorderRadius.circular(10)),
+        errorBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+            borderRadius: BorderRadius.circular(10)),
+        focusedErrorBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+            borderRadius: BorderRadius.circular(10)),
+      ),
+    );
+  }
+}
